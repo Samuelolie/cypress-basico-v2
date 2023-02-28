29148f92f2a599a99012081ab02f1452f99abdd1
= <=>
2023-02-28 09:47:03 -0300

    implementação novos testes

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -128,4 +128,19 @@ describe('Central de Atendimento ao Cliente TAT', () => {
             })
     });
 
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+        cy.get('a')
+            .should('have.attr', 'target', '_blank')
+    });
+
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+        cy.get('a')
+            .invoke('removeAttr', 'target')
+            .click()
+        cy.get('#title')
+            .should('contain', 'CAC TAT - Política de privacidade')
+        cy.contains('Talking About Testing')
+            .should('be.visible')
+    });
+
 })
---
+++ cypress/integration/privacy.spec.js
@@ -0,0 +1,8 @@
+it('testa a página da política de privacidade de forma independente', () => {
+    cy.visit('./src/privacy.html')
+
+    cy.get('#title')
+        .should('contain', 'CAC TAT - Política de privacidade')
+    cy.contains('Talking About Testing')
+        .should('be.visible')
+});
